ab95b8a2a76d4fd97fc0e0b360b7395b3a406fec
lys3269 <lys3269@dgu.ac.kr>
2023-04-19 01:35:06 +0900

    비밀정보보호_1

--- index.js
+++ index.js
@@ -15,20 +15,27 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! 헤헤");
+  res.send("Hello World! 헤헤 메롱롱");
 });
 
-app.post("/register", (req, res) => {
-  //회원 가입 할때 필요한 정보들을 client에서 가져오면
-  //그것들을 데이터 베이스에 넣어준다.
+app.post("/register", async (req, res) => {
+  //회원가입시 필요 정보를 client에서 가져오면
+  //데이터베이스에 삽입한다
+
+  //body parser를 통해 body에 담긴 정보를 가져온다
   const user = new User(req.body);
 
-  user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true,
+  //mongoDB 메서드, user모델에 저장
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
     });
-  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
