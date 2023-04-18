015655f5554674502c7c2723b31d8e69eebda133
lys3269 <lys3269@dgu.ac.kr>
2023-04-19 00:39:58 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "/mongodb+srv://yunseo3269:<dldbstj3269!>@boilerplate.a85fptm.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
@@ -20,5 +18,18 @@ app.get("/", (req, res) => {
   res.send("Hello World! 헤헤");
 });
 
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//mongodb+srv://yunseo3269:<password>@boilerplate.a85fptm.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://yunseo3269:dldbstj3269!@boilerplate.a85fptm.mongodb.net/?retryWrites=true&w=majority
